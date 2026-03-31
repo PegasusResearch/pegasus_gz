177ee626cecbc4e4960c68fd12e0c67715796d77
Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
2026-03-31 17:05:54 +0100

    fix: taguspark collision and default vehicle launch file

--- launch/vehicles/default_vehicle.launch.py
+++ launch/vehicles/default_vehicle.launch.py
@@ -105,23 +105,25 @@ def launch_vehicle(context, *args, **kwargs):
     # Check if we need to launch the Pegasus GNC stack
     pegasus_gnc_launchfile = LaunchConfiguration('pegasus_gnc_launchfile').perform(context)
 
-    # # Check if the specified launch file exists
+    # 3. Check if the specified launch file exists
     if pegasus_gnc_launchfile != 'none':
+        
+        # Try to grab the specified launch file inside the pegasus package
         pegasus_launchfile_path = os.path.join(get_package_share_directory('pegasus'), 'launch', pegasus_gnc_launchfile)
         if not os.path.isfile(pegasus_launchfile_path):
             raise FileNotFoundError(f"Pegasus GNC launch file '{pegasus_launchfile_path}' does not exist. Please provide a valid launch file or specify pegasus_gnc_launchfile:=none.")
 
-    # # 3. Spawn the Pegasus GNC stack (if applicable)
-    pegasus_launch = IncludeLaunchDescription(
-        # Grab the launch file for the mavlink interface
-        PythonLaunchDescriptionSource(pegasus_launchfile_path),
-        # Define costume launch arguments/parameters used for the mavlink interface
-        launch_arguments={
-            'id': str(vehicle_id), 
-            'namespace': LaunchConfiguration('vehicle_ns').perform(context),
-            'connection': 'udp://:' + str(14540 + port_increment)
-        }.items(),
-    )
+        # Spawn the Pegasus GNC stack (if applicable)
+        pegasus_launch = IncludeLaunchDescription(
+            # Grab the launch file for the mavlink interface
+            PythonLaunchDescriptionSource(pegasus_launchfile_path),
+            # Define costume launch arguments/parameters used for the mavlink interface
+            launch_arguments={
+                'id': str(vehicle_id), 
+                'namespace': LaunchConfiguration('vehicle_ns').perform(context),
+                'connection': 'udp://:' + str(14540 + port_increment)
+            }.items(),
+        )
 
     # Return all launch actions
     launch_actions = [px4_sitl, spawn_entity]
@@ -177,7 +179,7 @@ def generate_launch_description():
         DeclareLaunchArgument('pegasus_gnc_launchfile', default_value='none', description='Whether to launch the Pegasus GNC stack alongside the vehicle'),
         DeclareLaunchArgument('x', default_value='0.0', description='X position expressed in ENU'),
         DeclareLaunchArgument('y', default_value='0.0', description='Y position expressed in ENU'),
-        DeclareLaunchArgument('z', default_value='0.0', description='Z position expressed in ENU'),
+        DeclareLaunchArgument('z', default_value='0.3', description='Z position expressed in ENU'),
         DeclareLaunchArgument('R', default_value='0.0', description='Roll orientation expressed in ENU'),
         DeclareLaunchArgument('P', default_value='0.0', description='Pitch orientation expressed in ENU'),
         DeclareLaunchArgument('Y', default_value='0.0', description='Yaw orientation expressed in ENU'),
